05b9cbdf3a44e974ec906ef6450c7d8fe1a7e1a7
Andres Zambrano <azambrano13@hotmail.com>
2019-03-15 11:56:38 -0500

    comentarios

--- definitions.py
+++ definitions.py
@@ -15,45 +15,45 @@ import numpy as np
 import random
 
 
-#Define una ficha del juego
+# Define una ficha del juego
 class ficha():
     def __init__(self,lado_1,lado_2):
-        #Número 1 de la ficha
+        # Número 1 de la ficha
         self.num_1=lado_1
-        #Número 2 de la ficha
+        # Número 2 de la ficha
         self.num_2=lado_2
-        #Determina si la ficha es doble
+        # Determina si la ficha es doble
         self.es_doble=True if self.num_1==self.num_2 else False
-    #Asigna números a la ficha
+    # Asigna números a la ficha
     def __str__(self):
         l=str(self.num_1)+"/"+str(self.num_2)
         return l
 
-#Define las caracterísitcas del juego
+# Define las caracterísitcas del juego
 class juego():
     def __init__(self,N):
-        #Tablero
+        # Tablero
         self.tablero=[]
-        #Números que se pueden jugar
+        # Números que se pueden jugar
         self.numeros_posibles=()
-        #Cantidad de jugadores, se mantendrá normalmente en 4
+        # Cantidad de jugadores, se mantendrá normalmente en 4
         self.nume_jugadores=N
-        #Fichas del juego
+        # Fichas del juego
         self.fichas = []
-        #Define las 28 fichas del juego
+        # Define las 28 fichas del juego
         for i in range(7):
             for j in range(i, 7):
                 self.fichas.append(ficha(i,j))
 
 
-    #Inicia el  juego
+    # Inicia el juego dando las fichas de cada jugador
     def iniciar(self):
-        #Se hace un arreglo aleatorio de fichas
+        # Se hace un arreglo aleatorio de fichas
         fichas_jugadores=[]
         fichas_por_jugador=int(len(self.fichas)/self.N)
         fichas=np.random.shuffle(self.fichas)
 
-        #Se entregan fichas a cada jugador
+        # Se entregan fichas a cada jugador
         for i in range(self.N):
 
             fichas_jugadores.append(fichas[:fichas_por_jugador])
@@ -63,32 +63,35 @@ class juego():
         return fichas_jugadores
 
 
-
+    # Reinicia el tablero
     def reset(self):
         tablero=[]
 
+    # Hace que el jugador juegue, decidiendo dónde poner una ficha válida, y actualiza el tablero
     def jugada_jugador(self, ficha,):
-       numeros_p=np.array([self.numeros_posibles[0],self.numeros_posibles[1]])
-       numeros_ficha1=np.array([ficha.num_1,ficha.num_2])
-       numeros_ficha2=np.array([ficha.num_2,ficha.num_1])
-       numero_in=0
-       numero_out=0
-       if sum(numeros_p==numeros_ficha1)==0:
-           numero_in=numeros_ficha2[np.argmin(numeros_p==numeros_ficha2)]
-           numero_out = np.argmax(numeros_p == numeros_ficha2)
-       else:
-           numero_in = numeros_ficha2[np.argmin(numeros_p == numeros_ficha1)]
-           numero_out = np.argmax(numeros_p == numeros_ficha1)
+        # Números que puede jugar
+        numeros_p=np.array([self.numeros_posibles[0],self.numeros_posibles[1]])
+        # Orden 1 de ficha
+        numeros_ficha1=np.array([ficha.num_1,ficha.num_2])
+        # Orden 2 de ficha
+        numeros_ficha2=np.array([ficha.num_2,ficha.num_1])
+        numero_in=0
+        numero_out=0
+        if sum(numeros_p==numeros_ficha1)==0:
+            numero_in=numeros_ficha2[np.argmin(numeros_p == numeros_ficha2)]
+            numero_out = np.argmax(numeros_p == numeros_ficha2)
+        else:
+            numero_in = numeros_ficha2[np.argmin(numeros_p == numeros_ficha1)]
+            numero_out = np.argmax(numeros_p == numeros_ficha1)
+
+        self.numeros_posibles[numero_out]=numero_in
+
+        self.tablero.append(ficha)
+
+        return self.tablero
 
 
-       self.numeros_posibles[numero_out]=numero_in
-
-       self.tablero.append(ficha)
-
-       return self.tablero
-
-
-
+# Define jugador que utiliza RL para actuar
 class Jugador_re(nn.Module):
     def __init__(self,dims_states,dim_action,hidden_layers:list):
         super(Jugador_re, self).__init__()
